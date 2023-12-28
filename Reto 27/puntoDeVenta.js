6e0ef9e1d9e9177e3cfbdd9f3944fb756649935d
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-28 13:28:43 -0500

    Caso exitoso 6

--- Reto 27/puntoDeVenta.js
+++ Reto 27/puntoDeVenta.js
@@ -28,7 +28,9 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
          */
     //6. Invocar a calcularDescuento y lo que devuelve guardar en la variable valorDescuento
+    valorDescuento = calcularValorDescuento(precioProducto,porcentajeDescuento);
     //7. Mostrar el resultado en el componente lblDescuento
+    mostrarTexto("lblDescuento", valorDescuento);
         /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10
