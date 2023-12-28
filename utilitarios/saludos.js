03754510a52505d7b6caa2c455516bf55409b242
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-28 12:33:59 -0500

    Implementar las funciones mostrar Texto e Imagenes

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -1,5 +1,5 @@
 
-recurperarTexto = function(idComponente){
+recurperarTexto = function (idComponente) {
     let componente;
     let valorIngresado;
     componente = document.getElementById(idComponente);
@@ -7,15 +7,16 @@ recurperarTexto = function(idComponente){
     return valorIngresado;
 };
 saludar = function () {
-  let nombre = recurperarTexto("txtNombre");
-  let apellido = recurperarTexto("txtApellido");
-  let edad = recuperarInt("txtEdad");
-  let estatura = recuperarFloat("txtEstatura");
-  console.log(edad);
-  console.log(estatura);
+    let nombre = recurperarTexto("txtNombre");
+    let apellido = recurperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarFloat("txtEstatura");
+    let mensajeBienvenida = "Bienvenido" + nombre + apellido;
+    mostrarTexto("lblResultado", mensajeBienvenida);
+    mostrarImagen("imgSaludo", "./images/pato.gif");
 };
 
-recuperarInt = function (idComponente){
+recuperarInt = function (idComponente) {
     let valorCaja = recurperarTexto(idComponente);
     let valorEntero = parseInt(valorCaja);
     return valorEntero;
@@ -23,7 +24,20 @@ recuperarInt = function (idComponente){
 };
 
 recuperarFloat = function (idComponente) {
-  let valorCaja = recurperarTexto(idComponente);
-  let valorDecimal = parseFloat(valorCaja);
-  return valorDecimal;
+    let valorCaja = recurperarTexto(idComponente);
+    let valorDecimal = parseFloat(valorCaja);
+    return valorDecimal;
+};
+
+mostrarTexto = function (idComponente, mensaje) {
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.innerText = mensaje;
+
+};
+
+mostrarImagen = function(idComponente,rutaImagen){
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.src = rutaImagen;
 };
